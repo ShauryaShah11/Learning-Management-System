cd010ff3d6aedc7921188b96008199b48b4ffd9e
ShauryaShah11 <shahshaurya11@gmail.com>
2023-12-21 22:00:09 +0530

    Modified Course Material model

--- backend/models/CourseMaterial.js
+++ backend/models/CourseMaterial.js
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
+const subsectionSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  type: { type: String, required: true }, // e.g., video, pdf, quiz, article
+  url: { type: String, required: true }, // URL or CMS reference
+  fileSize: { type: Number }, // Optional for specific types
+  duration: { type: Number }, // Optional for videos
+  order: { type: Number, default: 0 }, // Resource order
+  completedByStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
+});
+const parentTitleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  subsections: [subsectionSchema],
+});
+
 const courseMaterialSchema = new mongoose.Schema({
   course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
-  resources: [{
-    type: { type: String, required: true }, // e.g., video, pdf, quiz, article
-    title: { type: String, required: true },
-    description: { type: String }, // Optional
-    url: { type: String, required: true }, // URL or CMS reference
-    fileSize: { type: Number }, // Optional for specific types
-    duration: { type: Number }, // Optional for videos
-    order: { type: Number, default: 0 }, // Resource order
-    completedByStudents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
-  }],
+  parentTitles: [parentTitleSchema], // Multiple parent titles containing subsections
 });
 courseMaterialSchema.index({ course: 1 });
-courseMaterialSchema.index({ type: 1, order: 1 }); // Example for filtering
+
 const CourseMaterial = mongoose.model('CourseMaterial', courseMaterialSchema);
 
-module.exports = CourseMaterial;
+export default CourseMaterial;
